3c8e51157edce8eda96d6b4e9237ab90313e318a
Alemshewit <alemshewitweldearegay@gmail.com>
2014-09-10 12:56:54 -0600

    triviagame

--- TriviaGame/Program.cs
+++ TriviaGame/Program.cs
@@ -11,26 +11,49 @@ namespace TriviaGame
     {
         static void Main(string[] args)
         {
+            //call greet function to greet player
+            Greeting();
             //The logic for your trivia game happens here
             List<Trivia> AllQuestions = GetTriviaList();
+            int correctAnswer = 0;
+            int wrongAnswer = 0;
+            bool playing = true;
+            Random rng = new Random();
+
+            while (playing == true)
+            {
+                int question = rng.Next(1, 5001);
+                
+            }
+
+
         }
 
 
         //This functions gets the full list of trivia questions from the Trivia.txt document
         static List<Trivia> GetTriviaList()
         {
+           
+
             //Get Contents from the file.  Remove the special char "\r".  Split on each line.  Convert to a list.
             List<string> contents = File.ReadAllText("trivia.txt").Replace("\r", "").Split('\n').ToList();
 
             //Each item in list "contents" is now one line of the Trivia.txt document.
             
             //make a new list to return all trivia questions
+
             List<Trivia> returnList = new List<Trivia>();
             // TODO: go through each line in contents of the trivia file and make a trivia object.
             //       add it to our return list.
             // Example: Trivia newTrivia = new Trivia("what is my name?*question");
             //Return the full list of trivia questions
             return returnList;
+        }
+
+        static void Greeting()
+        {
+           Console.WriteLine("Hello! Here are fun trivia games for you to play!");
+
         }
     }
 }
--- TriviaGame/Trivia.cs
+++ TriviaGame/Trivia.cs
@@ -12,13 +12,28 @@ namespace TriviaGame
         
         //The Trivia Object will have 2 properties
         // at a minimum, Question and Answer
+        public string Question { get; set; }
+        public string Answer { get; set; }
 
         //The Constructor for the Trivia object will
         // accept only 1 string parameter.  You will
         // split the question from the answer in your
         // constructor and assign them to the Question
         // and Answer properties
+        public Trivia(string triviaQandA)
+        {
+            //creat a new List variable to hold our questions and answers
+            List<string> triviaQuestions = new List<string>();
+            //split the argument into to indexes along the *
+            //and populate the new list with the new values
+            triviaQuestions = triviaQandA.Split('*').ToList();
 
+            //the property question will get the first index in the list 
+            this.Question = triviaQuestions[0];
+            //the property answer will get the second index in the list
+            this.Answer = triviaQuestions[1];
+
+        }
 
     }
 }
